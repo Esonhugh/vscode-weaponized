81e24d15a0f0a23bb8e91fc0f4cfcbffcd79f605
Esonhugh <esonhughoutside@gmail.com>
2025-07-01 21:52:27 +0800

    update: on delete file resync

--- src/variableProcessor/index.ts
+++ src/variableProcessor/index.ts
@@ -1,17 +1,13 @@
 import * as vscode from "vscode";
 import { targetFilePattern } from "../global/const";
 import {
-  getCodeblock,
   ProcessMarkdownFileToWorkspaceState,
   ProcessWorkspaceStateToEnvironmentCollects,
 } from "./syncHostMarkdown";
 import { logger } from "../global/log";
-import { parseHostsYaml, parseUserCredentialsYaml } from "../model";
 import { Context } from "../global/context";
 
-export async function registerVariablesWatcher(
-  context: vscode.ExtensionContext
-) {
+async function init(context: vscode.ExtensionContext) {
   // clean update the extension's workspace state
   Context.HostState = [];
   Context.UserState = [];
@@ -36,7 +32,9 @@ export async function registerVariablesWatcher(
   } else {
     logger.warn("No workspace found, skipping workspace state processing.");
   }
+}
 
+function filewatcher(context: vscode.ExtensionContext): vscode.FileSystemWatcher {
   let filewatcher = vscode.workspace.createFileSystemWatcher(targetFilePattern);
   context.subscriptions.push(
     filewatcher.onDidChange(async (file) => {
@@ -49,39 +47,16 @@ export async function registerVariablesWatcher(
     }),
     filewatcher.onDidDelete(async (file) => {
       logger.info(`Watched file deleted: ${file.fsPath}`);
-      /*
       let wksp = vscode.workspace.getWorkspaceFolder(file);
-      if (wksp) {
-        logger.debug(`Processing workspace: ${wksp.name}`);
-        logger.trace(`Removing hosts from workspace state for file: ${file.fsPath}`);
-        const content = (await vscode.workspace.fs.readFile(file)).toString();
-        let hostCodes = await getCodeblock(content, "host")
-        for (const code of hostCodes) {
-          let hosts = parseHostsYaml(code)
-          if (hosts.length > 0) {
-            logger.info(`Removing hosts from workspace state: ${hosts.map(h => h.hostname).join(", ")}`);
-            let oldHostList = Context.HostState || [];
-            oldHostList = oldHostList.filter(h => !hosts.some(host => host.hostname === h.hostname));
-            Context.HostState = oldHostList;
-            logger.debug(`Updated hosts in workspace state: ${Context.HostState.map(h => h.hostname).join(", ")}`);
-          }
-        }
-
-        logger.trace(`Removing users from workspace state for file: ${file.fsPath}`);
-        let userCodes = await getCodeblock(content, "credentials")
-        for (const code of userCodes) {
-          let users = parseUserCredentialsYaml(code)
-          if (users.length > 0) {
-            logger.info(`Removing users from workspace state: ${users.map(u => u.user).join(", ")}`);
-            let oldUserList = Context.UserState || [];
-            oldUserList = oldUserList.filter(u => !users.some(user => `${user.login}/${user.user}` === `${u.login}/${u.user}`));
-            Context.UserState = oldUserList;
-            logger.debug(`Updated users in workspace state: ${Context.UserState.map(u => `${u.login}/${u.user}`).join(", ")}`);
-          }
-        }
-        await ProcessWorkspaceStateToEnvironmentCollects(wksp);
-      } 
-        */
-    }),
+      await init(context);
+    })
   );
+  return filewatcher;
+}
+
+export async function registerVariablesWatcher(
+  context: vscode.ExtensionContext
+) {
+  await init(context);
+  filewatcher(context);
 }
