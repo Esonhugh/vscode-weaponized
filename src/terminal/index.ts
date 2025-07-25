330c8ec48f2394f539610de69d717a8279a7ab29
esonhugh <esonhughoutside@gmail.com>
2025-07-25 17:00:52 +0800

    update: terminal log settings

--- src/terminal/index.ts
+++ src/terminal/index.ts
@@ -1,6 +1,12 @@
 import * as vscode from "vscode";
 import { registerTerminalForCapture } from "./record_append";
+import { logger } from "../global/log";
 
 export function registerTerminal(context: vscode.ExtensionContext) {
-  registerTerminalForCapture();
+  if(vscode.workspace.getConfiguration("weaponized").get<boolean>("terminal-log.enabled", false)) {
+    logger.info("Registering terminal for capture...");
+    registerTerminalForCapture();
+  } else {
+    logger.info("Terminal logging is disabled.");
+  }
 }
--- src/terminal/record_append.ts
+++ src/terminal/record_append.ts
@@ -1,8 +1,7 @@
 import * as vscode from "vscode";
 import { logger } from "../global/log";
-import 'fs';
+import "fs";
 import { appendFileSync } from "fs";
-
 
 async function ForceFileExist(file: vscode.Uri) {
   try {
@@ -14,17 +13,25 @@ async function ForceFileExist(file: vscode.Uri) {
   } catch (error) {
     logger.info(`File ${file.fsPath} does not exist, creating it...`);
     await vscode.workspace.fs.writeFile(file, Buffer.from(""));
-  } 
+  }
 }
 
 export function registerTerminalForCapture() {
-  logger.info("Registering terminal for capture...");
   if (!vscode.workspace.workspaceFolders) {
-    logger.error("No workspace folders found. Cannot register terminal for capture.");
+    logger.error(
+      "No workspace folders found. Cannot register terminal for capture."
+    );
     return;
   }
-  let logFile = vscode.Uri.joinPath(vscode.workspace.workspaceFolders?.[0]?.uri, ".vscode/terminal.log");
-  
+  let fp = vscode.workspace
+    .getConfiguration("weaponized")
+    .get<string>("terminal-log.path", "<default_logfile>");
+  if (fp === "<default_logfile>") {
+    let workspaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    fp = `${workspaceFolder}/.vscode/.terminal.log`;
+  }
+  let logFile = vscode.Uri.parse(fp);
+
   vscode.window.onDidStartTerminalShellExecution(
     async (event: vscode.TerminalShellExecutionStartEvent) => {
       await ForceFileExist(logFile);
@@ -34,21 +41,23 @@ export function registerTerminalForCapture() {
       let cmd = event.execution.commandLine.value;
       let cwd =
         event.execution.cwd?.fsPath || event.shellIntegration?.cwd || "unknown";
-      logger.debug(
-        `user@${cwd} [${startTime.toString()}] > ${cmd}\n`
-      );
+      logger.debug(`user@${cwd} [${startTime.toString()}] > ${cmd}\n`);
       appendFileSync(
         logFile.fsPath,
         `user@${cwd} [${startTime.toString()}] > ${cmd}\n`
       );
 
+      const loglevel = vscode.workspace
+        .getConfiguration("weaponized")
+        .get<string>("terminal-log.level", "command-only");
+      if (loglevel === "command-only") {
+        return; // Only log the command, no further action needed
+      }
+
       let stream = await event.execution.read();
       let old: string = "";
       for await (const streamPart of stream) {
-        appendFileSync(
-          logFile.fsPath,
-          `${old}\n`
-        );
+        appendFileSync(logFile.fsPath, `${old}\n`);
         old = streamPart;
       }
     }
