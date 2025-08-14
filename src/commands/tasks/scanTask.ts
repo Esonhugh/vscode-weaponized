d5326abf3447a20604b82e45e057d2afb50aa6fa
esonhugh <esonhughoutside@gmail.com>
2025-08-14 16:43:20 +0800

    update: target with directly input

--- src/commands/tasks/scanTask.ts
+++ src/commands/tasks/scanTask.ts
@@ -16,39 +16,46 @@ export const scanCommand: callback = async (args: any) => {
   if (!target) {
     if (!selectTargets || selectTargets.length === 0) {
       vscode.window.showErrorMessage("No hosts found to scan.");
-      return;
-    }
-    let selected: Host | undefined = undefined;
-    if (selectTargets.length !== 1) {
-      let list: string[] = [];
-      selectTargets.forEach((host) => {
-        list.push(`${host.hostname} (${host.ip})`);
+      target = await vscode.window.showInputBox({
+        placeHolder: "Enter a target (ip/domain/...etc) to scan",
       });
-      target = await vscode.window.showQuickPick(list, {
-        placeHolder: "Select a host to scan",
-      });
-      if (!target) {
+    } else {
+      let selected: Host | undefined = undefined;
+      if (selectTargets.length !== 1) {
+        let list: string[] = [];
+        selectTargets.forEach((host) => {
+          list.push(`${host.hostname} (${host.ip})`);
+        });
+        target = await vscode.window.showQuickPick(list, {
+          placeHolder: "Select a host to scan",
+        });
+        if (!target) {
+          vscode.window.showErrorMessage(
+            "No target selected. Operation cancelled."
+          );
+          return;
+        }
+        selected = selectTargets.find(
+          (host) => `${host.hostname} (${host.ip})` === target
+        );
+      } else {
+        selected = selectTargets[0];
+      }
+
+      if (!selected) {
         vscode.window.showErrorMessage(
-          "No target selected. Operation cancelled."
+          "Selected target not found in host list."
         );
         return;
       }
-      selected = selectTargets.find(
-        (host) => `${host.hostname} (${host.ip})` === target
+      let options: string[] = Array.from(
+        new Set([selected.hostname, selected.ip, ...selected.alias])
       );
-    } else {
-      selected = selectTargets[0];
-    }
-    
-    if (!selected) {
-      vscode.window.showErrorMessage("Selected target not found in host list.");
-      return;
-    }
-    let options: string[] = Array.from(new Set([selected.hostname, selected.ip, ...selected.alias]));
 
-    target = await vscode.window.showQuickPick(options, {
-      placeHolder: "Select an option to scan",
-    });
+      target = await vscode.window.showQuickPick(options, {
+        placeHolder: "Select an option to scan",
+      });
+    }
   }
   if (!target) {
     vscode.window.showErrorMessage("No target selected. Operation cancelled.");
